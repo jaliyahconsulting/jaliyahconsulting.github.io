0c11d9132701e3a5fbb49afe8577e47660d7b736
Cain <cainwatson@users.noreply.github.com>
2017-11-16 07:04:25 -0600

    complete refactor

---
+++ scripts/gallery.js
@@ -0,0 +1,19 @@
+
+// $(() => {
+//   $('.slider').slick({
+//     autoplay: false,
+//     arrows: false,
+//     dots: false,
+//     infinite: false,
+//     asNavFor: '.slider-nav',
+//   });
+//   $('.slider-nav').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     dots: false,
+//     infinite: false,
+//     focusOnSelect: true,
+//     asNavFor: '.slider',
+//   });
+// });
