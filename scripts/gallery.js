f7446bd160b394491779b7ee2d7820a4bd574428
Makiyah Cormick <retrofied23@gmail.com>
2017-11-25 02:06:13 +0000

    made gallery a slider

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,19 +1,41 @@
+var $;
 
-// $(() => {
-//   $('.slider').slick({
-//     autoplay: false,
-//     arrows: false,
-//     dots: false,
-//     infinite: false,
-//     asNavFor: '.slider-nav',
-//   });
-//   $('.slider-nav').slick({
-//     slidesToShow: 5,
-//     slidesToScroll: 1,
-//     arrows: false,
-//     dots: false,
-//     infinite: false,
-//     focusOnSelect: true,
-//     asNavFor: '.slider',
-//   });
-// });
+const gallery = {
+  init() {
+    this.imgs = Array.from($('.hover-img-container'));
+    this.arrows();
+    this.bottomSelect();
+  },
+  currImg: 0,
+  changeImage(newImageIndex) {
+    $(this.imgs[this.currImg]).removeClass('current-img');
+    $(this.imgs[newImageIndex]).addClass('current-img');
+
+    $(`[data-index=${this.currImg}]`).removeClass('bottom-current');
+    $(`[data-index=${newImageIndex}]`).addClass('bottom-current');
+    this.currImg = newImageIndex;
+  },
+  arrows() {
+    $('.last-img').click(() => {
+      let changeTo = this.currImg;
+      if (this.currImg - 1 < 0) changeTo = this.imgs.length;
+      this.changeImage(changeTo - 1);
+    });
+
+    $('.next-img').click(() => {
+      let changeTo = this.currImg;
+      if (this.currImg + 1 >= this.imgs.length) changeTo = -1;
+      this.changeImage(changeTo + 1);
+    });
+  },
+  bottomSelect() {
+    Array.from($('.bottom-image')).forEach((e, i) => $(e).attr('data-index', i));
+
+    $('.bottom-image').click(function() {
+      const index = $(this).attr('data-index');
+      gallery.changeImage(Number(index));
+    });
+  },
+};
+
+$(() => gallery.init());
