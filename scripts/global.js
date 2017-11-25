1bfac0bdb76bb52e3c650f8a75b9a2f82d5c4e2b
Makiyah Cormick <retrofied23@gmail.com>
2017-11-25 08:10:21 +0000

    fixed nav changing sizes

--- scripts/global.js
+++ scripts/global.js
@@ -1,12 +1,19 @@
+var $;
 
 const jáliyah = {
   init() {
     this.eventListeners();
+    this.dropDown();
   },
   eventListeners() {
     $('.hamburger').click(() => $('.nav').slideToggle());
     $('.nav-item.dropdown').click(() => window.location = 'about.html');
-  }
+  },
+  dropDown() {
+    $('.dropdown-menu').css('width', $('.about-dropdown').css('width'));
+    $(window).resize(() =>
+      $('.dropdown-menu').css('width', $('.about-dropdown').css('width')));
+  },
 };
 
 $(() => jáliyah.init());
